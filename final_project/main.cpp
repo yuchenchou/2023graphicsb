810ba3dd1fca2416f6ecc327f6790ba5c14fb53c
yuchenchou <melodychou999@gmail.com>
2023-05-11 16:12:08 +0800

    week13

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h"
+GLMmodel *head=NULL;
+GLMmodel *body=NULL;
+GLMmodel *rightup=NULL;
+GLMmodel *rightdown=NULL;
+int show[4]={1,0,0,0};
 float teapotX =0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key ,int x,int y){
+    if(key=='0')show[0]=!show[0];
+    if(key=='1')show[1]=!show[1];
+    if(key=='2')show[2]=!show[2];
+    if(key=='3')show[3]=!show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if (head==NULL){
+        head=glmReadOBJ("model/head.obj");
+        body=glmReadOBJ("model/body.obj");
+        rightup=glmReadOBJ("model/rightup.obj");
+        rightdown=glmReadOBJ("model/rightdown.obj");
+    }
     glPushMatrix();
-        glTranslatef(teapotX, teapotY, 0);
-        glutSolidTeapot( 0.3 );
+        glScalef(0.3,0.3,0.3);
+        if ( show [0]) glmDraw(head,GLM_MATERIAL);
+        if ( show [1]) glmDraw(body,GLM_MATERIAL);
+        if ( show [2]) glmDraw(rightup,GLM_MATERIAL);
+        if ( show [3]) glmDraw(rightdown,GLM_MATERIAL);
+
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -23,15 +46,15 @@ void mouse(int button, int state, int x, int y)
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-    if(fin==NULL){
-        fclose(fout);
-        fin = fopen("file4.txt", "r");
-    }
-    fscanf(fin, "%f%f", &teapotX, &teapotY);
-    display();
-}
+///void keyboard(unsigned char key, int x, int y)
+///{
+  ///  if(fin==NULL){
+    ///    fclose(fout);
+    ///    fin = fopen("file4.txt", "r");
+    ///}
+    ///fscanf(fin, "%f%f", &teapotX, &teapotY);
+    ///display();
+///}
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
